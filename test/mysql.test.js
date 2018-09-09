a0a6730eb648580ac70c1746dfe898d30d1a11e3
Daniel Jhin Yoo <danieljyoo@gmail.com>
2018-09-08 22:23:46 -0700

    updated module to funcmatic 2.0 format

--- test/mysql.test.js
+++ test/mysql.test.js
@@ -1,58 +1,96 @@
 require('dotenv').config()
-var Funcmatic = require('@funcmatic/funcmatic')
+var funcmatic = require('@funcmatic/funcmatic')
 var MySQLPlugin = require('../lib/mysql')
-var mysqlconf = {
+
+funcmatic.use(MySQLPlugin, {
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE
-}
-
-var handler = Funcmatic.wrap(async (event, context, { mysql }) => {
-  await mysql.query('DELETE FROM users')
-  var user = { 
-    id: "TEST-USER-ID", 
-    created_at: new Date(), 
-    updated_at: new Date() 
-  }
-  var insertRes = await mysql.query('INSERT INTO users SET ?', user)
-  var q = { id: "TEST-USER-ID" }
-  var selectRes = await mysql.query('SELECT * FROM users WHERE ?', q)
-  var deleteRes = await mysql.query('DELETE FROM users WHERE ?', { id: "TEST-USER-ID" } )
-  return {
-    statusCode: 200,
-    insertRes, selectRes
-  }    
 })
 
-
-afterAll(async () => {
-  await MySQLPlugin.disconnectFromDatabase()
-})
+// var handler = Funcmatic.wrap(async (event, context, { mysql }) => {
+//   await mysql.query('DELETE FROM users')
+//   var user = { 
+//     id: "TEST-USER-ID", 
+//     created_at: new Date(), 
+//     updated_at: new Date() 
+//   }
+//   var insertRes = await mysql.query('INSERT INTO users SET ?', user)
+//   var q = { id: "TEST-USER-ID" }
+//   var selectRes = await mysql.query('SELECT * FROM users WHERE ?', q)
+//   var deleteRes = await mysql.query('DELETE FROM users WHERE ?', { id: "TEST-USER-ID" } )
+//   return {
+//     statusCode: 200,
+//     insertRes, selectRes
+//   }    
+// })
 
 describe('Request', () => {
+  var plugin = null
   beforeEach(async () => {
-    Funcmatic.clear()
+    funcmatic = funcmatic.clone()
+    plugin = funcmatic.getPlugin('mysql')
   })
-  it ('should get an uncached connection', async () => {
-    Funcmatic.use(MySQLPlugin, mysqlconf)
+  afterEach(async () => {
+    await funcmatic.teardown()
+  })
+
+  it ('should create an uncached connection', async () => {
     var event = { }
     var context = { }
-    var ret = await handler(event, context)
-    console.log(JSON.stringify(ret.insertRes, null, 2))
-    var results = ret.selectRes[0]
-    var fields = ret.selectRes[1]
-    expect(results.length).toBe(1)
-    expect(results[0]).toMatchObject({
-      id: "TEST-USER-ID"
+    await funcmatic.invoke(event, context, async (event, context, { mysql }) => {
+      expect(mysql).toBeTruthy()
     })
-    expect(MySQLPlugin.cachedConn).toBeFalsy()
+    expect(plugin.cachedConn).toBeFalsy()
   })
-  it ('should get an cached connection', async () => {
-    Funcmatic.use(MySQLPlugin, Object.assign(mysqlconf, { cache: true }))
+  it ('should create a cached connection', async () => {
+    var funcmaticCached = funcmatic.clone()
+    funcmaticCached.clear()
+    funcmaticCached.use(MySQLPlugin, {
+      host: process.env.MYSQL_HOST,
+      user: process.env.MYSQL_USER,
+      password: process.env.MYSQL_PASSWORD,
+      database: process.env.MYSQL_DATABASE,
+      cache: true
+    })
+    plugin = funcmaticCached.getPlugin('mysql')
     var event = { }
     var context = { }
-    await handler(event, context)
-    expect(MySQLPlugin.cachedConn).toBeTruthy()
+    await funcmaticCached.invoke(event, context, async (event, context, { mysql }) => {
+      expect(mysql).toBeTruthy()
+    })
+    expect(plugin.cachedConn).toBeTruthy()
+    await funcmaticCached.teardown()
+    expect(plugin.cachedConn).toBeFalsy()
+  })
+  it ('should perform basic SQL operations', async () => {
+    var event = { }
+    var context = { }
+    await funcmatic.invoke(event, context, async (event, context, { mysql }) => {
+      var id = `TEST-USER-ID-${new Date().getTime()}`
+      var user = { 
+        id, 
+        created_at: new Date(), 
+        updated_at: new Date() 
+      }
+      var insertRes = await mysql.query('INSERT INTO users SET ?', user)
+      expect(insertRes.length).toBe(2)
+      expect(insertRes[0]).toMatchObject({
+        affectedRows: 1
+      })
+      
+      var q = { id }
+      var selectRes = await mysql.query('SELECT * FROM users WHERE ?', q)
+      expect(selectRes[0].length).toBe(1)
+      expect(selectRes[0][0]).toMatchObject({
+        id
+      })
+      var deleteRes = await mysql.query('DELETE FROM users WHERE ?', q)
+      expect(deleteRes.length).toBe(2)
+      expect(deleteRes[0]).toMatchObject({
+        affectedRows: 1
+      })
+    })
   })
 }) 
